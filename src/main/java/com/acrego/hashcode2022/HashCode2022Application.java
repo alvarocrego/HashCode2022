8d68ac7a620ebb8679999a1c8aca2aa579bedafb
Alvaro Crego <alvarocrego@users.noreply.github.com>
2022-02-24 18:17:15 +0100

    Initial commit

---
+++ src/main/java/com/acrego/hashcode2022/HashCode2022Application.java
@@ -0,0 +1,13 @@
+package com.acrego.hashcode2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashCode2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HashCode2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/acrego/hashcode2022/HashCode2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acrego.hashcode2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashCode2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
